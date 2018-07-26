da534a639cbca6c2fa075525dba9f6e2c38e20fd
Shraddha Murarka <shraddham@metricstream.com>
2018-07-26 10:04:42 +0530

    mileage calc first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  cuurCost:any;
+  lastKms:any;
+  currKms:any;
+  kmsdriven:any;
+  totalmile:any;
+  lastPetFill:any;
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+    console.log(this.cuurCost);
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  Calculate(){
+    console.log(this.cuurCost);
+    console.log(this.lastKms);
+    console.log(this.currKms);
+    console.log(this.lastPetFill);
+    this.kmsdriven = this.currKms - this.lastKms;
+    this.totalmile = this.kmsdriven / this.lastPetFill;
+    console.log(this.totalmile);
   }
 
 }
